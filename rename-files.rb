a4bc2628f911e31008d5951d9e9db02417528930
Andrea Cipriani <andrea.cipriani@soundcloud.com>
2017-04-23 17:20:13 +0200

    Loop to get file base name of all the files

--- rename-files.rb
+++ rename-files.rb
@@ -1,8 +1,13 @@
 #!/usr/bin/ruby
+
 if ARGV.empty?
   puts "Usage: ./rename-files $directory"
   exit 1
 end
 
 dir = ARGV[0]
-puts dir
+
+# Dir[dir + "/*"]
+Dir[dir + "/*"].select{ |f| File.file? f }.map{ |f| File.basename f }.each do |file_basename|
+    puts file_basename
+end
